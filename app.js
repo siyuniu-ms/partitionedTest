100c5fb40cd704b1a47974988aac0f585d2f624e
siyuniu-ms <siyuniu@microsoft.com>
2024-03-14 18:20:46 -0700

    for web

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    // Set a cookie directly
+    res.cookie('myCookie', 'myValue', {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None',
+        maxAge: 24 * 60 * 60 * 1000,
+        partitioned: true
+    });
+    res.send('Cookie set!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
