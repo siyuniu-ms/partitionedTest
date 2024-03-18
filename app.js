415562a27599cf70bd82c2dbfd384a2e736dc48e
siyuniu-ms <siyuniu@microsoft.com>
2024-03-18 15:17:45 -0700

    4

--- app.js
+++ app.js
@@ -30,6 +30,6 @@ app.post('/', (req, res) => {
     });
     res.send('Cookie set!');
   });
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
